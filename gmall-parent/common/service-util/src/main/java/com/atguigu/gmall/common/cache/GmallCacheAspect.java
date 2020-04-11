3e61e29208f028763dcd9e7d3c039cde97799481
lxteacher <112624349@qq.com>
2020-04-11 16:49:48 +0800

    分布式锁+Aop切面实现类修改BUG

--- gmall-parent/common/service-util/src/main/java/com/atguigu/gmall/common/cache/GmallCacheAspect.java
+++ gmall-parent/common/service-util/src/main/java/com/atguigu/gmall/common/cache/GmallCacheAspect.java
@@ -39,17 +39,20 @@ public class GmallCacheAspect {
         MethodSignature signature = (MethodSignature) pjp.getSignature();//签名==当前方法的  public  返回值  包名+ 类名 + 方法名 + 入参
         Method method = signature.getMethod();
         GmallCache gmallCache = method.getAnnotation(GmallCache.class);
+        //返回值 类型
+        Class returnType = signature.getReturnType();
+
         String prefix = gmallCache.prefix();
         //入参
         Object[] args = pjp.getArgs();
         //缓存Key值
         String key = prefix + Arrays.asList(args).toString();
         //1:从缓存中查询
-        Object o = redisTemplate.opsForValue().get(key);
+        String o = (String) redisTemplate.opsForValue().get(key);
         if(null != o){
             //2:有  直接返回
             log.info("缓存中有要查询的数据");
-            return o;
+            return JSONObject.parseObject(o,returnType);
         }
         //3:没有 查询数据库  保存缓存一份  击穿
 
@@ -63,15 +66,12 @@ public class GmallCacheAspect {
                 //穿透
                 if(null == proceed){
                     Object o1 = new Object();
-                    //Object 必须实现序列化接口 转成Json格式字符串
-                    //Object 入参是对象转Json格式字符串  入参直接就是Json格式字符串呢？
-                    //Object 类型由自己完成Json的转换   结果Json格式字符串
                     String json = JSONObject.toJSONString(o1);
                     redisTemplate.opsForValue().set(key,json,5,TimeUnit.MINUTES);
-                    return o1;
+                    return JSONObject.parseObject("{}",returnType);
                 }else{
-                    //String json = JSONObject.toJSONString(proceed);
-                    redisTemplate.opsForValue().set(key,proceed,RedisConst.SKUKEY_TIMEOUT,TimeUnit.SECONDS);
+                    String json = JSONObject.toJSONString(proceed);
+                    redisTemplate.opsForValue().set(key,json,RedisConst.SKUKEY_TIMEOUT,TimeUnit.SECONDS);
                     return proceed;
                 }
             }else{
@@ -85,6 +85,6 @@ public class GmallCacheAspect {
                 lock.unlock();
             }
         }
-        return new Object();//空结果
+        return JSONObject.parseObject(new String(),returnType);//空结果
     }
 }
